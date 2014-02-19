f1b0da71421c20a3c68842977b19f4ee3a63e6f7
unknown <andrecampos.webdesign@gmail.com>
2014-02-19 20:19:44 -0300

    geral

---
+++ js/website.js
@@ -0,0 +1,21 @@
+angular.module('website', []).
+	config(function($routeProvider) {
+
+		$routeProvider.
+			when('/home', {templateUrl:'templates/home.html'}).
+			when('/areas', {templateUrl:'templates/areas-template.html'}).
+			when('/profile', {templateUrl:'templates/profile-template.html'}).
+			when('/contact', {templateUrl:'templates/contact-template.html'}).
+			when('/login', {templateUrl:'templates/login.html'}).
+			when('/book', {templateUrl:'templates/book.html'}).
+			otherwise({redirectTo:'/home', templateUrl:'templates/home.html'});
+	});
+
+
+function MainCtrl($scope, $location) {
+	$scope.setRoute = function(route) {
+		$location.path(route);
+	}
+}
+
+
